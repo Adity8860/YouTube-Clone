71504c5507aa8113ed1158a770dc3c38f515ecff
AdityaGaur008 <your-email@example.com>
2024-09-15 23:22:46 +0530

    few more

---
+++ src/utils/AsyncHandler.js
@@ -0,0 +1,18 @@
+const asyncHandler  = (asyncHandler) => (req, res, next) => {
+    Promise.resolve(asyncHandler(req, res, next)).
+    catch((err) => next(err))
+}
+
+// Higher order funcrion to handle async errors
+// const asyncHandler = (fn) => async (req, res, next) => { 
+//   try {
+//     await fn(req, res, next);
+//   } catch (error) {
+//     res.status(error.code || 500).json({
+//       success: false,
+//       message: error.message,
+//     });
+//   }
+// };
+
+export { asyncHandler };
