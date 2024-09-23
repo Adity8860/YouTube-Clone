436d45fc014d2e33bf38f2349c2c0b276e86db8d
Aditya Gaur <gauradi355@gmail.com>
2024-09-23 12:25:32 +0530

    commit

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,31 @@
+import { ApiError } from "../utils/ApiError";
+import { asyncHandler } from "../utils/asyncHandler";
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.model.js";
+
+export const verifyJWT = asyncHandler(async (req, res, next) => {
+  try {
+    const token =
+      req.cookies?.accessToken ||
+      req.headers.authorization?.replace("Bearer ", " ");
+  
+    if (!token) {
+      throw new ApiError(400, "unautherized request");
+    }
+  
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+  
+    const user = await User.findById(decodedToken?.id).select(
+      "-password -REFRESH_TOKEN_SECRET"
+    );
+  
+    if (!user) {
+      throw new ApiError(401, "Invalid access");
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    throw new ApiError(401, "Invalid access");
+    
+  }
+});
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,12 +1,25 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
-router.route("/register").post(upload.fields([
-    { name: "avatar",
-        maxCount:1
+router.route("/register").post(
+  upload.fields([
+    { name: "avatar", maxCount: 1 },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
 
-}, {}]), registerUser);
+router.route("/login").post(loginUser);
+
+// secured Routes
+
+router.route("/logout").post(verifyJWT, logoutUser);
+
 
 export default router;
