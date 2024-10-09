8d90932156bdf6b91ecb04be99803118a5ff5fed
Aditya Gaur <gauradi355@gmail.com>
2024-10-10 02:33:34 +0530

    optimic

--- src/app.js
+++ src/app.js
@@ -17,8 +17,8 @@ app.use(express.static("public"));
 app.use(cookieParser());
 
 //routes import
-import userRouter from "./routes/user.routes.js";
-import tweetRouter from "./routes/tweet.routes.js";
+import userRouter from "../src/routes/user.route.js";
+import tweetRouter from "../src/routes/tweet.route.js";
 //routes declaration
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/tweets", tweetRouter);
